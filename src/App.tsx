fb3a72914d01f8d5afb5a2c118e9f43a70a52e21
ut5tw <sheredan2003@gmail.com>
2024-10-08 11:06:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import Experience from './Experience'
 
 const App = () => {
   return (
-    <div className="h-dvh bg-[#030B22] flex justify-center items-center text-4xl text-white gap-10">
-      <Experience />
+    <div className="h-dvh bg-[#030B22] text-4xl text-white flex flex-col">
+      <div className="flex-shrink-0 flex items-center justify-center">Cube</div>
+      <div className="flex-1">
+        <Experience />
+      </div>
     </div>
   )
 }
